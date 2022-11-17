80ad3d4310e3c0564e1fb487ad03d06504bb35bc
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-11-17 19:34:46 +0100

    Add helmet

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
 
+app.use(helmet());
 app.use(
   session({
     secret: process.env.SESS_SECRET,
